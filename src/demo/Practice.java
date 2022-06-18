32a1b8a17ac734da411ec68e2ab4fc7f9294c619
balkrushnaingale <107671821+balkrushnaingale@users.noreply.github.com>
2022-06-18 15:25:22 +0530

    new update

--- src/demo/Practice.java
+++ src/demo/Practice.java
@@ -4,7 +4,8 @@ public class Practice {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("Hello this is Jio ");
+System.out.println("Hello this is Jio project ");
+		
 	}
 
 }
